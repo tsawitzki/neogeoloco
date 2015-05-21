e7a382e8333fa6093746e0fa918b1a9fade3d290
TS <tools@letrip.de>
2015-05-21 16:21:01 +0200

    fading sequencing moved to function

--- sequence.js
+++ sequence.js
@@ -11,16 +11,59 @@ function fadeItemOut(i) {
 }
 
 
-function initSequence() {
-  $('h1, p, #btn-start').animate({opacity: '0'}, txtanmo, 'swing', function() {
-    $('h1').css('display', 'none');
-    $('h2').html('[ Getting your location ]');
-    $('p').html('Locating ...');
-    fadeItemIn('h2');
-    window.setTimeout(function(){fadeItemIn('p');}, 500);
-    
-    // do the geo stuff here, so I have all variables and infos ready, i can throw the errors just in case and all I have to do now is flow through the rest of the sequence, all output set, just launch the clips at the end of the animation cycle
-    
-    
-  });
+
+
+if ("geolocation" in navigator) {
+  console.log("OK: Browser geolocation API is available");
+} else {
+  console.log("WARNING: Browser geolocation API is NOT available");
 }
+
+
+
+
+
+// geoFindMe();
+
+
+var home_faded_out = false;
+
+function fadeInLoadscreen() {
+  if (home_faded_out === true) {
+    console.log('all faded');
+  }
+}
+
+function fadeHomeOut() {
+  $('h1').fadeTo(txtanmo, 0);
+  $('p').delay(200).fadeTo(txtanmo, 0);
+  $('button').delay(400).fadeTo(txtanmo, 0, function(){
+    $('h1').remove();
+    $('#stage').prepend('<h2></h2>');    
+    intro_out = true;
+    fadeInLoadscreen();
+  });
+    
+
+}
+
+fadeHomeOut();
+
+
+
+
+
+
+
+
+
+
+
+// do the geo stuff here, so I have all variables and infos ready, i can throw the errors just in case and all I have to do now is flow through the rest of the sequence, all output set, just launch the clips at the end of the animation cycle
+
+// render coordinates
+
+// stems howl init via vars
+
+// play it
+
